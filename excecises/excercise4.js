d33d5cc61ba0f5ee2ae8975b722ace004198785f
JuanPabloCaicedo <juanpablo10oficial@gmail.com>
2024-09-12 15:59:27 -0500

    Ejercicio 4 hecho

--- excecises/excercise4.js
+++ excecises/excercise4.js
@@ -6,12 +6,30 @@ Si la operación no es válida, debe retornar "Operación no reconocida".
 Recuerde que "No se puede dividir entre 0".
 */
 function calculadora(operacion, num1, num2) {
-    // TODO: Resolver
+    switch(operacion) {
+        case "+":
+        return num1 + num2
+        case "-":
+        return num1 - num2
+        case "*":
+        return num1 * num2
+        case "/":
+        if (num2 === 0){
+            return "No es posible dividir entre 0"
+        } else {
+            return num1/num2
+        }
+
+        default: 
+        return "Error en el operador"
+    }
+   
 }
 
 console.log("-----------------------------------")
 console.log("EJERCICIO 4")
 console.log("-----------------------------------")
-console.log(calculadora("suma", 5, 3));         // 8
-console.log(calculadora("division", 10, 0));    // "No se puede dividir entre 0"
-console.log(calculadora("multiplicacion", 2, 4)); // 8
+console.log(calculadora("+", 5, 3));         // 8
+console.log(calculadora("/", 10, 0));    // "No se puede dividir entre 0"
+console.log(calculadora("*", 2, 4)); // 8
+console.log(calculadora("/", 200,5)); 
