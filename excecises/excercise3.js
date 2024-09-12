2de46035f2e8ce0e951ca31881061d5740069a28
JuanPabloCaicedo <juanpablo10oficial@gmail.com>
2024-09-12 15:41:43 -0500

    Ejercicio 3 hecho

--- excecises/excercise3.js
+++ excecises/excercise3.js
@@ -5,7 +5,18 @@ que reciba dos números como argumentos y retorne el mayor de los dos.
 Si los números son iguales, debe retornar "Los números son iguales".
 */
 function mayorDeDos(num1, num2) {
-    // TODO: Resolver
+    if(num1 > num2){
+        return num1
+    }
+    else if (num2 > num1) {
+        return num2
+
+    } else if (num1 === num2) {
+        return "Los números son iguales"
+        
+    } else {
+        return "error"
+    }
 }
 
 console.log("-----------------------------------")
@@ -13,3 +24,4 @@ console.log("EJERCICIO 3")
 console.log("-----------------------------------")
 console.log(mayorDeDos(5, 10)); // 10
 console.log(mayorDeDos(8, 8));  // "Los números son iguales"
+console.log(mayorDeDos(2,9))
