0133830280454dab44feb0f5ab75dc7000403086
JuanPabloCaicedo <juanpablo10oficial@gmail.com>
2024-09-12 20:15:15 -0500

    Ejercicio 10 hecho

--- excecises/excercise10.js
+++ excecises/excercise10.js
@@ -5,11 +5,16 @@ que reciba un año como argumento y retorne "Es bisiesto" si el año es divisibl
 o si es divisible entre 400. En caso contrario, retorne "No es bisiesto".
 */
 function esBisiesto(anio) {
-    // TODO: Resolver
+    if ((anio % 4 === 0 && anio % 100 !== 0) || (anio % 400 === 0)) {
+        return "Es bisiesto";
+    } else {
+        return "No es bisiesto";
+    }
 }
 
-console.log("-----------------------------------")
-console.log("EJERCICIO 10")
-console.log("-----------------------------------")
+console.log("-----------------------------------");
+console.log("EJERCICIO 10");
+console.log("-----------------------------------");
 console.log(esBisiesto(2020)); // "Es bisiesto"
 console.log(esBisiesto(2100)); // "No es bisiesto"
+console.log(esBisiesto(2010))
